b5ef1dce48c9b8bc65e598fa3b8ff9ce0a1ea589
Irina Romjalis <iromjalis@gmail.com>
2021-10-02 12:32:18 +0300

    cast as button to click

---
+++ src/components/Cast/Cast.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+//import { Test } from './Cast.styles';
+
+const Cast = props => <div className="CastWrapper">Cast content</div>;
+
+Cast.propTypes = {
+  // bla: PropTypes.string,
+};
+
+Cast.defaultProps = {
+  // bla: 'test',
+};
+
+export default Cast;

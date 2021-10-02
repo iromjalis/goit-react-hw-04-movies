283ecb6ddcfd5b5eb2e929d8b05814075ec8f58a
Irina Romjalis <iromjalis@gmail.com>
2021-10-02 12:30:58 +0300

    reviews components just as button to go to film review

---
+++ src/components/Reviews/Reviews.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+//import { Test } from './Reviews.styles';
+
+const Reviews = props => <div className="ReviewsWrapper">Reviews content</div>;
+
+Reviews.propTypes = {
+  // bla: PropTypes.string,
+};
+
+Reviews.defaultProps = {
+  // bla: 'test',
+};
+
+export default Reviews;

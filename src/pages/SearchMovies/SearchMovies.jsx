9a10ba0715925ae8c757ebb2592283f3d3745858
Irina Romjalis <iromjalis@gmail.com>
2021-10-04 11:25:40 +0300

    from searchbar component to searchmovies page

--- src/pages/SearchMovies/SearchMovies.jsx
+++ src/pages/SearchMovies/SearchMovies.jsx
@@ -1,14 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Searchbar from '../../components/Searchbar/Searchbar';
+import { render } from '@testing-library/react';
+import Api from '../../Service/Api';
+import FilmList from '../../components/FilmList/FilmList';
 //import { Test } from './SearchMovies.styles';
 
-const SearchMovies = props => (
-  <div className="SearchMoviesWrapper">
-    <h1>SearchMovies</h1>
-    <Searchbar />
-  </div>
-);
+class SearchMovies extends Component {
+  state = {
+    query: '',
+    films: [],
+  };
+  async componentDidMount() {
+    if (this.props.location.state !== null) {
+      const response = await Api.fetchMoviesByQuery(
+        this.props.location.state.query,
+      );
+      this.setState({ query: this.props.location.state.query });
+    }
+  }
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    await Api.fetchMoviesByQuery(this.state.query).then(res =>
+      this.setState({ films: res }),
+    );
+  };
+  handleChange = e => {
+    this.setState({ query: e.target.value });
+  };
+  render() {
+    const { query } = this.state;
+    return (
+      <>
+        <form className="SearchMoviesWrapper" onSubmit={this.handleSubmit}>
+          <label>
+            <input
+              onChange={this.handleChange}
+              type="text"
+              placeholder="typing movie title..."
+              value={query}
+            ></input>
+          </label>
+          <button type="submit">Search</button>
+        </form>
+        <FilmList
+          films={this.state.films}
+          history={this.props.history}
+          query={query}
+        />
+      </>
+    );
+  }
+}
 
 SearchMovies.propTypes = {
   // bla: PropTypes.string,

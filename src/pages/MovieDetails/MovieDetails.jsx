3955c2f528e24ba5875580e921ca7927ca68aa9c
Irina Romjalis <iromjalis@gmail.com>
2021-10-05 01:04:44 +0300

    Update MovieDetails.jsx

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,69 +1,75 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Route } from 'react-router';
-import Reviews from '../../components/Reviews/Reviews';
-import Cast from '../../components/Cast/Cast';
-import { NavLink, Link } from 'react-router-dom';
+import {
+  useParams,
+  Link,
+  Route,
+  useRouteMatch,
+  useHistory,
+} from 'react-router-dom';
+import Loader from 'react-loader-spinner';
 import Api from '../../Service/Api';
 import styles from './MovieDetails.module.css';
+import { useEffect, useState, lazy, Suspense } from 'react';
+import PropTypes from 'prop-types';
+const Cast = lazy(() => import('../../components/Cast/Cast'));
+const Reviews = lazy(() => import('../../components/Reviews/Reviews'));
 
-class Moviedetails extends Component {
-  url = this.props.match.url;
-  state = {
-    film: {},
-  };
-  async componentDidMount() {
-    const id = this.props.match.params.id;
-    this.setState({ id });
-    const response = await Api.fetchMoviesById(id);
-    this.setState({ film: response });
-  }
-  handleGoBack = () => {
-    const from = this.props.history.push(this.props.location.state.from, {
-      query: this.props.location.state.query,
-    });
-  };
-  render() {
-    const { film } = this.state;
-    const { title, overview, poster_path } = film;
-    return (
-      <>
-        <p>
-          <button onClick={this.handleGoBack}>Go Back</button>
-        </p>
-        <img
-          src={`https://image.tmdb.org/t/p/w500${poster_path}`}
-          alt="poster"
-          width="450"
-        />
-        <h3>{title}</h3>
-        <p>{overview} </p>
-        <hr />
-        <div className={styles.CastAndReviewsBtn}>
-          <Link
-            to={{
-              pathname: `${this.props.match.url}/cast`,
-              state: this.props.location.state,
-            }}
-            className={styles.CastBtn}
-          >
-            Cast
-          </Link>
-          <Link
-            to={{
-              pathname: `${this.props.match.url}/reviews`,
-              state: this.props.location.state,
-            }}
-            className={styles.ReviewsBtn}
-          >
-            Review
-          </Link>
-        </div>
+function Moviedetails() {
+  const { id } = useParams();
+  const { url, path } = useRouteMatch();
+  const history = useHistory();
+  const [film, setFilm] = useState({});
+  const { title, poster_path, overview } = film;
+  const { ref, search } = history.location.state;
 
-        <Route path={`${this.props.match.path}/reviews`} component={Reviews} />
-        <Route path={`${this.props.match.path}/cast`} component={Cast} />
-      </>
-    );
-  }
+  useEffect(() => {
+    const response = Api.fetchMoviesById(id).then(r => setFilm(r));
+  }, [id]);
+
+  return (
+    <>
+      <p>
+        <Link to={`${ref}${search}` ? `${ref}${search}` : '/'}>
+          <button type="button">Go back</button>
+        </Link>
+      </p>
+      <img
+        src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+        alt="poster"
+        width="450"
+      />
+      <h3>{title}</h3>
+      <p>{overview} </p>
+      <hr />
+      <div className={styles.CastAndReviewsBtn}>
+        <Link
+          to={{
+            pathname: `${url}/cast`,
+            state: {
+              ref: ref,
+              search: search,
+            },
+          }}
+          className={styles.CastBtn}
+        >
+          Cast
+        </Link>
+        <Link
+          to={{
+            pathname: `${url}/reviews`,
+            state: {
+              ref: ref,
+              search: search,
+            },
+          }}
+          className={styles.ReviewsBtn}
+        >
+          Review
+        </Link>
+      </div>
+
+      <Route path={`${path}/reviews`} component={Reviews} />
+      <Route path={`${path}/cast`} component={Cast} />
+    </>
+  );
 }
 export default Moviedetails;

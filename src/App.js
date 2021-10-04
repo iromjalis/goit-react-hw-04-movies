72e0e31fc7b5c22ffcfe88351fa9e7848496561e
Irina Romjalis <iromjalis@gmail.com>
2021-10-04 11:29:39 +0300

    route path and redirect added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component, lazy, Suspense } from 'react';
+import { Redirect } from 'react-router';
 import Loader from 'react-loader-spinner';
 import { Route, Switch } from 'react-router-dom';
 import routes from './routes';
+//components
 import Navigation from './components/Navigation/Navigation';
 import Home from './pages/Home/Home';
 import SearchMovies from './pages/SearchMovies/SearchMovies';
 import MovieDetails from './pages/MovieDetails/MovieDetails';
 import NotFound from './pages/NotFound/NotFound';
+
 import css from './App.css';
 
 function App() {
@@ -14,11 +17,11 @@ function App() {
     <div className="App">
       <Navigation />
       <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/movies" exact component={SearchMovies} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/movies" component={SearchMovies} />
         <Route path="/movies/:id" component={MovieDetails} />
         <Route component={NotFound} />
-        {/* <Redirect to={{ pathname: '/' }} /> */}
+        <Redirect to="/" />
       </Switch>
     </div>
   );
